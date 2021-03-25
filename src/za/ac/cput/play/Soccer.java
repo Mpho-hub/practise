9fe9c76f3acda40566fe59310bd762ceb84f2b07
sefolokof <sefolokof@fusi>
2021-03-25 10:36:26 +0200

    First Commit

---
+++ src/za/ac/cput/play/Soccer.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.play;
+
+/**
+ *
+ * @author sefolokof
+ */
+public class Soccer {
+    public static int integerSum(int a, int b){
+        return a + b;
+    }
+    
+}
